b5f6d551a8bf4c6d353b968f597e29566cf4e95d
nsamjm <nsmajm@gmail.com>
2020-01-16 21:00:14 +0600

    Some config modification & product model, migration , controller

---
+++ database/migrations/2020_01_16_144805_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('supplier_user_id')->comment('Id of supplier who supplied this product');
+            $table->string('product_name');
+            $table->integer('quantity')->comment('Supplied quantity');
+            $table->integer('sku');
+            $table->timestamps();
+            $table->foreign('supplier_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                @can('view-inventory')
+                  Inventory
+                @endcan
+                @can('view-supplies')
+                  Supplies
+                @endcan
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +21,6 @@
                         </div>
                     @endif
 
-                    You are logged in!
                 </div>
             </div>
         </div>
